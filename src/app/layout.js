336c35f9513b0e2ccb97a324e8d3a7a162a578e5
Md. Juyel Hossen <juyelhossen0093@gmail.com>
2025-02-18 03:07:51 +0600

    edited page title

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,9 +18,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>Document</title>
-      </head>
       <body>
         <div className="container mx-auto px-1 sm:px-0 overflow-hidden min-h-screen">
           {children}
